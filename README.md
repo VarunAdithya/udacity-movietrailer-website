ace561805a85021c52c3bca84200477cd62ca48b
Varun <varun.s@volvo.com>
2019-03-27 08:54:24 +0530

    reformatted the code

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Project Description:
 
         This project is about building a website that hosts our favourite movie's & tvshows.
 	Of course in the one that I am working on will display my favourite
-        movies & tv shows :-). Let's have a look, shall we?
+        movies & tv shows: -). Let's have a look, shall we?
 
 What's Included:
 
@@ -17,12 +17,12 @@ What's Included:
 
 To Run This Project:
 
-        To run this project on your computer, python 2.7.15 installation is needed. Create a folder 
-	named Video_Website, copy the files to the folder and run #python entertainment_center.py & BOOM!! 
-	The webpage is at your service where you can view your favorite (in this case, ,my favourite)
+        To run this project on your computer, python 2.7.15 installation is needed. Create a folder
+	# python entertainment_center.py & BOOM!!
+	named Video_Website, copy the files to the folder and run
+	The webpage is at your service where you can view your favorite(in this case, , my favourite)
 	movie title, poster, trailer link on youtube, duration, rating & so on.
 
 Creator:
 
 	-Varun
-
--- media.py
+++ media.py
@@ -1,49 +1,66 @@
 import webbrowser
 
+"""This class provides Title,Rating & Duration to Movies and TvShow sub-class"""
 
-    """This class provides Title,Rating & Duration to Movies and TvShow sub-class"""
 
 class Video():
     """
-    This is a parent class that has common inputs for movie & tv shows 
+    This is a parent class that has common inputs for movie & tv shows
     Arguments are Title, Duration, Genre & Rating
     """
+
     def __init__(self, title, duration, genre, rating):
         self.title = title
         self.duration = duration
         self.genre = genre
         self.rating = rating
-    
-    
+
+
 class Movie(Video):
     """
-    This is a child class named Movie that defines a class movie that is used to display information about your favourite Movie
-    Arguments are Titile, Duration, Genre, Rating, Poster_Image link & Youtube link
+    This is a child class named Movie that defines a class movie that is used to display
+    information about your favourite Movie Arguments are Titile, Duration, Genre, Rating,
+    Poster_Image link & Youtube link
     """
-    def __init__(self, title, movie_storyline, duration, genre, rating, poster_image, trailer_youtube):
-        Video.__init__(self,title, duration, genre, rating)
+
+    def __init__(
+            self,
+            title,
+            movie_storyline,
+            duration,
+            genre,
+            rating,
+            poster_image,
+            trailer_youtube):
+        Video.__init__(self, title, duration, genre, rating)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
 
-
 class TvShow(Video):
     """
-    This is a child class named TvShow that defines a class tvshows that is used to display information about your favorite TV Shows
-    Arguments are Titile, Duration, Genre, Rating, Poster_Image link & Youtube link
+    This is a child class named TvShow that defines a class tvshows that is used to display
+    information about your favorite TV Shows Arguments are Titile, Duration, Genre, Rating,
+    Poster_Image link & Youtube link
     """
-    def __init__(self, title, tv_storyline, duration, genre, rating, poster_image, trailer_youtube):
-        Video.__init__(self,title, duration, genre, rating)
+
+    def __init__(
+            self,
+            title,
+            tv_storyline,
+            duration,
+            genre,
+            rating,
+            poster_image,
+            trailer_youtube):
+        Video.__init__(self, title, duration, genre, rating)
         self.storyline = tv_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-
     def show_trailer(self):
         webrowser.open(self.trailer_youtube_url)
-        
